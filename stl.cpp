786596489b9dd04fb9d2545b641cbed25461a8dc
SakshiBha <sakshibharti49@gmail.com>
2024-04-21 21:27:50 +0530

    ---function sum---

--- stl.cpp
+++ stl.cpp
@@ -1,9 +1,24 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    int a=10,b=20,sum;
-    sum=a+b;
-    cout<<" sum of two number "<< sum;
+// int main(){
+//     int a=10,b=20,sum;
+//     sum=a+b;
+//     cout<<" sum of two number "<< sum;
+//     return 0;
+// }
+
+int sum(int a,int b)
+{
+    return a+b;
+}
+int main (){
+    int a,b;
+    cout<<" enter two numbers ";
+    cin>>a>>b;
+
+    int add = sum(a,b);
+    cout<<" sum of two numbers ";
+    cout<<add;
     return 0;
 }
