cd2b98bd7586e3c3a3871b4a4047069756298921
SakshiBha <sakshibharti49@gmail.com>
2024-04-23 00:21:03 +0530

    ---Book Allocation Problem---

--- stl.cpp
+++ stl.cpp
@@ -1,13 +1,5 @@
 #include<iostream>
 using namespace std;
-
-// int main(){
-//     int a=10,b=20,sum;
-//     sum=a+b;
-//     cout<<" sum of two number "<< sum;
-//     return 0;
-// }
-
 int sum(int a,int b)
 {
     return a+b;
